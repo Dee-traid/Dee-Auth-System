f4a23abbfd2a05ce1c46dc7929ed626c593cab09
Joshua Adejoh <adekelvin369@gmail.com>
2026-03-05 19:09:29 +0100

    New filed added to handle csrf token and minimal changes in the index page

---
+++ api/csrf_helper.php
@@ -0,0 +1,15 @@
+<?php
+
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+if(empty($_SESSION['csrf_token'])){
+	$_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+}
+
+function verifyCsrfToken($token){
+	return isset($_SESSION['csrf_token']) && hash_equals($_SESSION['csrf_token'], $token);
+}
+
+?>
